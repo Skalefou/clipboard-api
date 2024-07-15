f7da66b71f3e8767971d860f57bb90adac55ae82
Skalefou <skamsagar@gmail.com>
2024-07-15 16:02:16 +0200

    initial commit

---
+++ src/main/java/org/skalefou/clipboardapi/ClipboardApiApplication.java
@@ -0,0 +1,13 @@
+package org.skalefou.clipboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipboardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClipboardApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/skalefou/clipboardapi/ClipboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skalefou.clipboardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipboardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
